dcc3c725eef0fcec15ede08d91da422f0a9a3a2b
Zheer Ardalan <bestzheer@gmail.com>
2022-03-19 14:10:56 +0300

    Added settings

--- lib/Screens/MainScreens/SettingScreen.dart
+++ lib/Screens/MainScreens/SettingScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class settingScreen extends StatefulWidget {
   const settingScreen({Key? key}) : super(key: key);
@@ -8,23 +9,59 @@ class settingScreen extends StatefulWidget {
 }
 
 class _settingScreenState extends State<settingScreen> {
+  String dropdownvalue = 'English';
+
+  var items = ['English', 'Kurdish'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 20,
-          title: const Text(
-            'Settings',
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-          foregroundColor: Color(0xFF0B6E95), //#0B6E95
+      appBar: AppBar(
+        elevation: 20,
+        centerTitle: true,
+        title: const Text(
+          'Settings',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
-        body: Container(
-          color: Colors.white,
-          child: Text("Slaw Settings"),
-        ));
+
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        foregroundColor: Color(0xFF0B6E95), //#0B6E95
+      ),
+      body: Container(
+        child: SettingsList(
+          lightTheme: SettingsThemeData(settingsListBackground: Colors.white),
+          sections: [
+            SettingsSection(
+              tiles: <SettingsTile>[
+                SettingsTile(
+                  title: Text("Language"),
+                  leading: Icon(Icons.language),
+                  trailing: DropdownButton(
+                      value: dropdownvalue,
+                      items: items.map((String items) {
+                        return DropdownMenuItem(
+                          value: items,
+                          child: Text(items),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownvalue = newValue!;
+                        });
+                      }),
+                ),
+                SettingsTile.switchTile(
+                  onToggle: (value) {},
+                  initialValue: false,
+                  leading: Icon(Icons.sunny),
+                  title: Text('Change the theme'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
